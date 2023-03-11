7e6c59275e56b8c3d69fb109dd7c9bfb40ac2e68
Jordan Masone <jordan.masone@gfs.com>
2023-03-11 11:11:23 -0500

    Added logic.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hello, world!");
+// Get Current Number on Load
+let number = localStorage.getItem("number");
+
+// Single Function to update number
+const updateNumber = ({ target }) => {
+    if (target.id === "increase") {
+        number++
+    } else if (target.id === "decrease") {
+        number--
+    } else {
+        number = 0
+    }
+
+    // Update Number Storage and DOM
+    localStorage.setItem("number", number);
+    updateDom();
+}
+
+// Reusable update DOM function
+const updateDom = () => {
+    document.getElementById("number").innerText = number;
+}
+
+// Event Listeners
+const idArray = ["increase", "decrease", "reset"]
+for (let index = 0; index < idArray.length; index++) {
+    document.getElementById(idArray[index]).addEventListener("click", updateNumber)
+}
+
+// Update DOM on Load
+updateDom();
